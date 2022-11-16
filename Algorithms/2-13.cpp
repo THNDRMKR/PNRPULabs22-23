eba2d1c61002df1a33cd4bd56a9cced695a4bb8d
THNDRMKR <58326841+THNDRMKR@users.noreply.github.com>
2022-11-16 15:26:06 +0500

    Update 2-13.cpp

--- Algorithms/2-13.cpp
+++ Algorithms/2-13.cpp
@@ -4,10 +4,27 @@ using namespace std;
 
 int main()
 {
-    float a,b,c;
+    float a,b,c,d,x1,x2;
     cout << "Type a, b, c - coefficents of quadratic equation\n"; 
 
     cin >> a >> b >> c;
 
-    cout << "Roots are x1=" << (-b + sqrt(b*b-4*a*c))/(2*a) << " and x2=" << (-b - sqrt(b*b-4*a*c))/(2*a) << endl;;
+    d = pow(b,2) - 4*a*c
+    if (d > 0)
+    {
+        x1 = (-b+sqrt(d))/(2*a)
+        x2 = (-b-sqrt(d))/(2*a)
+        cout << "x1 = " << x1 << "x2 = " << x2 << endl;
+        
+    }
+    else if (d == 0) 
+    {
+        x1 = -b/(2*a)
+        cout << "x = " << x1 << endl;
+    }
+    else
+    {
+        cout << "No roots\n"
+    }
+    
 }
