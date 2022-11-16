09e26c60d22211de8466874e67163d174e5a2074
THNDRMKR <58326841+THNDRMKR@users.noreply.github.com>
2022-11-16 14:03:35 +0500

    Create 2-17.cpp

---
+++ Algorithms/2-17.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	int n, fact;
+	cout << "Type n - power of x: \n";
+	cin >> n;
+
+	fact = 1;
+	cout << "y = ";
+	for (int i = 1; i <= n; i++)
+	{
+		for (int j = 1; j <= i; j++)
+		{
+			fact *= j;
+		}
+		if (i != 1) cout << " + ";
+		cout << "x^" << i << "/" << fact;
+		fact = 1;
+	}
+}
