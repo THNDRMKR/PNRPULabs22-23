ddd220d968a9c063eb17e652e3dd8aa9f7ad879a
THNDRMKR <58326841+THNDRMKR@users.noreply.github.com>
2022-11-23 01:36:47 +0500

    Update 2-16.cpp
    changed a bit

--- Algorithms/2-16.cpp
+++ Algorithms/2-16.cpp
@@ -4,13 +4,14 @@ using namespace std;
 
 int main()
 {
-    int n, sum;
+    int n, result;
     cout << "Type n - numberof elements\n";
     cin >> n;
     sum = 0;
     for (int i = 1; i <= n; i++)
     {
-        sum+=i*pow(-1, i);
+        if (i%2==0) result+=i;
+        else result-=i;
     }
 
     cout << "Sum = " << sum << endl;
