d8423bc797e833a8d483238a23fda3bc87dcf9f9
THNDRMKR <yuldashev4amir@yandex.ru>
2022-11-23 01:21:16 +0500

    ?

--- Algorithms/2-17.cpp
+++ Algorithms/2-17.cpp
@@ -8,7 +8,6 @@ using namespace std;
 
 int main()
 {
-<<<<<<< HEAD
     int n;
 float x, fact, sum;
 
@@ -23,25 +22,3 @@ for (int i = 2; i <= n; i++)
     sum += (pow(x, i) / fact);
 }
 cout << sum << endl;
-}
-=======
-	int n, fact;
-	cout << "Type n - power of x: \n";
-	cin >> n;
-
-	fact = 1;
-	cout << "y = ";
-	for (int i = 1; i <= n; i++)
-	{
-		
-		if (i != 1) cout << " + ";
-		cout << "x^" << i << "/" << fact;
-		fact = 1;
-		for (int j = 1; j <= i; j++)
-		{
-			fact *= j;
-		}
-
-	}
-}
->>>>>>> eb6955cf1a17c6431444280b63f9685f464f69ae
