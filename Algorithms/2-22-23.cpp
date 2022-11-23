25924aaf6073a14bde002a0504b3dfa7d259f2a9
THNDRMKR <58326841+THNDRMKR@users.noreply.github.com>
2022-11-23 15:54:28 +0500

    Create 2-22-23.cpp

---
+++ Algorithms/2-22-23.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <cmath>
+
+using namespace std;
+
+int main()
+{
+  float n, a, max;
+  int i = 2;
+  int num = 1;
+  cin >> n;
+  
+  max = sin(n + 1/n)
+  while (i<=n)
+  }
+    if (a > max) 
+    {
+      max = a
+      num = i
+    }
+  {
+  cout << "Max element is " << max << "its number is" << num << endl;
+{
