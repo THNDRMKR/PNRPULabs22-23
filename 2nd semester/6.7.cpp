267eca2851d5324c8f3ec1e23f8a29d97b65faba
THNDRMKR <58326841+THNDRMKR@users.noreply.github.com>
2023-02-22 15:20:32 +0500

    Add files via upload

---
+++ 2nd semester/6.7.cpp
@@ -0,0 +1,24 @@
+﻿#include <iostream>
+#include <string>
+
+int main()
+{
+	char tmp, str[256];
+	std::cin.getline(str, 256);
+	for (int i = 0; i < std::string(str).length() - 1; i++)
+	{
+		for (int j = 0; j < std::string(str).length() - i - 1; j++)
+		{
+			if (str[j] > str[j + 1])
+			{
+				tmp = str[j];
+				str[j] = str[j + 1];
+				str[j + 1] = tmp;
+			}
+		}
+	}
+	for (int i = 0; i < std::string(str).length(); i++)
+	{
+		std::cout << str[i];
+	}
+}
